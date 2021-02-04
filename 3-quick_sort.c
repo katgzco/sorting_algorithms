d2390c11a0f37afe49dc4cea27a8d2a6b113d0fa
katgzco <katgzco@gmail.com>
2021-02-03 22:14:32 -0500

    Doc - Betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,14 +1,52 @@
 #include "sort.h"
 
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort
+ * @array: The array to be soted
+ * @size: Number of elements in @array
+ */
 
-void swap(int *value_1, int *value_2)
+void quick_sort(int *array, size_t size)
 {
-			int tmp = 0;
+	int end = 0;
 
-			tmp = *value_1;
-			*value_1 = *value_2;
-			*value_2 = tmp;
+	if (array && size >= 2)
+	{
+		end = (int) size - 1;
+		quick_sort_recursion(array, 0, end, size);
+	}
 }
+
+/**
+ * quick_sort_recursion - Function for call recursively quick_sort function
+ * @array: The array to be soted
+ * @size: Number of elements in @array
+ * @start: The first position in the array
+ * @end: the las position in the array
+ */
+
+void quick_sort_recursion(int *array, int start, int end, size_t size)
+{
+	int pi = 0;
+
+	if (start < end)
+	{
+		pi = partition(array, start, end, size);
+		quick_sort_recursion(array, start, pi - 1, size);
+		quick_sort_recursion(array, pi + 1, end, size);
+	}
+}
+
+/**
+ * partition -  sorts an array of integers in ascending order
+ * for the Quick sort
+ * @array: The array to be soted
+ * @size: Number of elements in @array
+ * @start: The first position in the array
+ * @end: the las position in the array
+ * Return: The position of the pivot
+ */
 int partition(int array[], int start, int end, size_t size)
 {
 	int pivot = *(array + end), pindex = (start - 1), i;
@@ -26,24 +64,16 @@ int partition(int array[], int start, int end, size_t size)
 	return (pindex + 1);
 }
 
-void quick_sort_recursion(int *array, int low, int high, size_t size)
+/**
+ * swap - Auxiliar function to swap values in an given index.
+ * @value_1: first indext to be swapped
+ * @value_2: second indext to be swapped
+ */
+void swap(int *value_1, int *value_2)
 {
-	int pi = 0;
+	int tmp = 0;
 
-	if (low < high)
-	{
-		pi = partition(array, low, high, size);
-		quick_sort_recursion(array, low, pi - 1, size);
-		quick_sort_recursion(array, pi + 1, high, size);
-	}
+	tmp = *value_1;
+	*value_1 = *value_2;
+	*value_2 = tmp;
 }
-
-void quick_sort(int *array, size_t size)
-{
-	if(array && (array + 1) && size >= 2)
-	{
-		int	high = (int) size -1;
-		quick_sort_recursion(array, 0, high, size);
-	}
-}
-
