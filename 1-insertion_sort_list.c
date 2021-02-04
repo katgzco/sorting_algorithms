a1873db7ec94b31eff6237c9f773d1516310040a
katgzco <katgzco@gmail.com>
2021-02-03 22:11:42 -0500

    Fix - change initial if for edge cases

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,26 +10,25 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current_next = NULL;
 
 	if (list != NULL && *list != NULL && (*list)->next)
+		return;
+	while (current)
 	{
-		while (current)
-		{
-			current_next = current->next;
-				while (current->prev && current->n < current->prev->n)
-				{
-					if (current->prev->prev != NULL)
-						current->prev->prev->next = current;
-					else
-						*list = current;
-					tmp_store = current->prev->prev;
-					current->prev->prev = current;
-					if (current->next != NULL)
-						current->next->prev = current->prev;
-					current->prev->next = current->next;
-					current->next = current->prev;
-					current->prev =  tmp_store;
-					print_list(*list);
-				}
-				current = current_next;
-		}
+		current_next = current->next;
+			while (current->prev && current->n < current->prev->n)
+			{
+				if (current->prev->prev != NULL)
+					current->prev->prev->next = current;
+				else
+					*list = current;
+				tmp_store = current->prev->prev;
+				current->prev->prev = current;
+				if (current->next != NULL)
+					current->next->prev = current->prev;
+				current->prev->next = current->next;
+				current->next = current->prev;
+				current->prev =  tmp_store;
+				print_list(*list);
+			}
+			current = current_next;
 	}
 }
