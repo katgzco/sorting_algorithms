a268f09ceb8363d711f32c5fdedcbe48359fb89d
katgzco <katgzco@gmail.com>
2021-02-03 22:10:27 -0500

    Fix - Add condition to swap the values

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,18 +10,21 @@ void selection_sort(int *array, size_t size)
 {
 	size_t iter_array = 0, min_comp = 0, iter_comp = 0, temp = 0;
 
-	if (array && size >= 2)
+	if (array == NULL && size < 2)
+		return;
+
+	for (iter_array = 0; iter_array < (size - 1); iter_array++)
 	{
+		min_comp = iter_array;
 
-		for (iter_array = 0; iter_array < (size - 1); iter_array++)
+		for (iter_comp = iter_array + 1; iter_comp < size; iter_comp++)
 		{
-			min_comp = iter_array;
+			if (*(array + iter_comp) < *(array + min_comp))
+				min_comp = iter_comp;
+		}
 
-			for (iter_comp = iter_array + 1; iter_comp < size; iter_comp++)
-			{
-				if (*(array + iter_comp) < *(array + min_comp))
-					min_comp = iter_comp;
-			}
+		if (min_comp != iter_array)
+		{
 			temp = *(array + iter_array);
 			*(array + iter_array) = *(array + min_comp);
 			*(array + min_comp) = temp;
